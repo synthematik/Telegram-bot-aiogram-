6a95f885b473a2252d9ca80209621943ba97be95
Garik <garfild_2003@mail.ru>
2023-05-12 22:27:12 +0300

    updated

--- todo.py
+++ todo.py
@@ -1,7 +1,6 @@
 #buttons = ["Город", "Новости"]
 #keyboard.add(*buttons)
 #await message.answer("Нажми: \nГород - для выбора своего города\nНовости - для просмотра актуальных новостей", reply_markup=keyboard)
-
 #f"Ваш город: ", reply_markup=keyboard)
 
 # cделать :продолжение: курсивом
